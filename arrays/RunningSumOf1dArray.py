e57f5e12f7fc0550bd58dcdd01409343cf0c0969
Urooj Zeeshan <ujji.zaidi777@gmail.com>
2026-09-13 17:14:19 +0500

    Implement running sum function in RunningSumOf1dArray

---
+++ arrays/RunningSumOf1dArray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        
+        result =[]
+        total = 0
+
+        for num in nums:
+            total = total + num
+            result.append(total)
+
+        return result
